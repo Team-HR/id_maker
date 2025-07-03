adff0c2ad71d1986fe874214131408be0f91814c
kimpoy31 <pinanonangkim000@gmail.com>
2025-07-03 15:40:55 +0800

    install daisyUi

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>;
+    return (
+        <div>
+            Dashboard <button className="btn btn-primary">Test</button>{" "}
+        </div>
+    );
 };
 
 export default Dashboard;
