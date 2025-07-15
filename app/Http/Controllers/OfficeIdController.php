af618d660ddd55dc2530c076fc5dc1ee5446bbd1
kimpoy31 <pinanonangkim000@gmail.com>
2025-07-15 09:49:54 +0800

    add Users table model and create seeds

--- app/Http/Controllers/OfficeIdController.php
+++ app/Http/Controllers/OfficeIdController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\OfficeId;
+use App\Models\OfficeId;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +15,53 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $departments = [
+            "City Accounting Office" => "cao",
+            "City Administrator's Office" => "cadmin",
+            "City Administrator's Office - Bids & Awards Committee Section" => "cadmin_bac",
+            "City Administrator's Office - Culture, Arts, Investment Promotion & Tourism Office" => "ctourism",
+            "City Agriculture Office" => "cagri",
+            "City Assessor's Office" => "cassess",
+            "City Budget Office" => "cbudget",
+            "City Civil Registry Office" => "ccivilreg",
+            "City Cooperative and Manpower Development Office" => "ccoopmd",
+            "City Engineering Office" => "ceng",
+            "City Environment & Natural Resources Office" => "cenro",
+            "City General Services Office" => "cgenserv",
+            "City Health Office" => "chealth",
+            "City Legal Office" => "clegal",
+            "City Mayor's Office" => "cmayor",
+            "City Planning and Development Office" => "cplan",
+            "City Public Safety Office" => "csafety",
+            "City Social Welfare & Development Office" => "cswdo",
+            "City Treasurer's Office" => "ctreas",
+            "City Veterinary Office" => "cveto",
+            "City Vice-Mayor's Office" => "cvmo",
+            "DepEd/Bayawan East Central School" => "deped_eastcs",
+            "DepEd/Bayawan National High School" => "deped_bnchs",
+            "Office of the Human Resource Management and Dev't" => "hrmdo",
+            "Internal Audit Services Section" => "ias",
+            "Information Technology Office" => "ito",
+            "Local Disaster and Risk Reduction Management Office" => "ldrrmo",
+            "Media Bureau" => "media",
+            "National Agencies" => "nat_agency",
+            "Office of the Secretary to the Sangguniang Panglungsod" => "sp_sec",
+            "Public Market & Slaughterhouse" => "pmarket_sh",
+            "Sangguniang Panglungsod" => "sp",
+        ];
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        foreach ($departments as $department => $username) {
+            $plainPassword = Str::random(8);
+
+            User::create([
+                'department' => $department,    
+                'username' => $username,
+                'password' => Hash::make($plainPassword),
+                'status' => 'active',
+            ]);
+
+            // dd("Username: {$username} | Password: {$plainPassword} | Department: {$department}");
+            $this->command->info("Username: {$username} | Password: {$plainPassword} | Department: {$department}");
+        }
     }
 }
