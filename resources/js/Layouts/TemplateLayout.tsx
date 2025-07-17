4982924504d78bccfdd7e497dd5c301d0b8982eb
kimpoy31 <pinanonangkim000@gmail.com>
2025-07-17 14:27:09 +0800

    handle logging out

--- resources/js/Layouts/TemplateLayout.tsx
+++ resources/js/Layouts/TemplateLayout.tsx
@@ -1,4 +1,4 @@
-import { usePage } from "@inertiajs/react";
+import { Link, usePage } from "@inertiajs/react";
 import { ChevronDown, LogOut, Settings, Settings2 } from "lucide-react";
 import React, { ReactNode } from "react";
 
@@ -32,15 +32,45 @@ const TemplateLayout = ({
                                 <Settings size={18} /> Settings
                             </a>
                         </li>
+
                         <li>
-                            <a className="font-bold gap-4 bg-base-200">
+                            {/* Open the modal using document.getElementById('ID').showModal() method */}
+                            <button
+                                className="font-bold gap-4 bg-base-200"
+                                onClick={() => {
+                                    const modal = document.getElementById(
+                                        "my_modal_2"
+                                    ) as HTMLDialogElement | null;
+                                    modal?.showModal();
+                                }}
+                            >
                                 <LogOut size={18} /> Logout
-                            </a>
+                            </button>
                         </li>
                     </ul>
                 </div>
             </div>
             <div className={className}>{children}</div>
+            <dialog id="my_modal_2" className="modal">
+                <div className="modal-box">
+                    <h3 className="font-bold text-lg">Logout?</h3>
+                    <p className="py-4">Are you sure you want to logout ?</p>
+                    <div className="flex justify-end gap-0.5">
+                        <form method="dialog">
+                            <button className="btn">Cancel</button>
+                        </form>
+                        <Link
+                            href={route("logout")}
+                            className="btn btn-primary"
+                        >
+                            Confirm
+                        </Link>
+                    </div>
+                </div>
+                <form method="dialog" className="modal-backdrop">
+                    <button>close</button>
+                </form>
+            </dialog>
         </div>
     );
 };
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\OfficeIdController;
 use App\Http\Controllers\ProfileController;
 use App\Models\OfficeId;
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -36,6 +37,16 @@ Route::middleware('auth')->group(function () {
     Route::post('/office-id/save', [OfficeIdController::class, 'save'])->name('office-id.save');
     Route::post('/office-id/update/{id}', [OfficeIdController::class, 'patch'])->name('office-id.patch');
     Route::get('/office-id/search', [OfficeIdController::class, 'search_office_id'])->name('office-id.search');
+
+    
+    Route::get('/logout', function (Request $request) {
+        Auth::logout(); // Logs the user out
+
+        $request->session()->invalidate(); // Invalidate the session
+        $request->session()->regenerateToken(); // Regenerate CSRF token
+
+        return to_route('login');
+    })->name('logout');
 });
 
 // require __DIR__.'/auth.php';
