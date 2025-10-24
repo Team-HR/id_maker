99373e2d564d6fd86e84e22432a3a6a97beb07f0
kimpoy31 <pinanonangkim000@gmail.com>
2025-10-24 14:16:13 +0800

    add new offices (obo, bplo)

--- database/seeders/AccountSeeder.php
+++ database/seeders/AccountSeeder.php
@@ -18,9 +18,18 @@ class AccountSeeder extends Seeder
         $account = ["configs" => ["xAxis" => 10, "yAxis" => 430, "fontSize" => 20], "roles" => ['editor']];
 
         User::create([
-            'department' => 'Character first',    
-            'username' => 'cf!',
-            'password' => Hash::make('cf1234'),
+            'department' => 'Business permit and licensing office',    
+            'username' => 'bplo!',
+            'password' => Hash::make('bplo1234'),
+            'configs' => $account['configs'],
+            'roles' => json_encode($account['roles']),
+            'status' => 'active',
+        ]);
+
+        User::create([
+            'department' => 'Office of the Building Official',    
+            'username' => 'obo!',
+            'password' => Hash::make('obo1234'),
             'configs' => $account['configs'],
             'roles' => json_encode($account['roles']),
             'status' => 'active',
